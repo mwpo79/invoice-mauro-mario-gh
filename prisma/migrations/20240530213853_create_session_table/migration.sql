e55b9be759f36b5d257ba3a3449ea143acd35680
Mauro Spinelli <m@lanificiodigitale.com>
2025-11-13 18:14:43 +0100

    prisma config

--- prisma/migrations/20240530213853_create_session_table/migration.sql
+++ prisma/migrations/20240530213853_create_session_table/migration.sql
@@ -1,11 +1,28 @@
 -- CreateTable
+-- CREATE TABLE "Session" (
+--     "id" TEXT NOT NULL PRIMARY KEY,
+--     "shop" TEXT NOT NULL,
+--     "state" TEXT NOT NULL,
+--     "isOnline" BOOLEAN NOT NULL DEFAULT false,
+--     "scope" TEXT,
+--     "expires" DATETIME,
+--     "accessToken" TEXT NOT NULL,
+--     "userId" BIGINT,
+--     "firstName" TEXT,
+--     "lastName" TEXT,
+--     "email" TEXT,
+--     "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+--     "locale" TEXT,
+--     "collaborator" BOOLEAN DEFAULT false,
+--     "emailVerified" BOOLEAN DEFAULT false
+-- );
 CREATE TABLE "Session" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "shop" TEXT NOT NULL,
     "state" TEXT NOT NULL,
     "isOnline" BOOLEAN NOT NULL DEFAULT false,
     "scope" TEXT,
-    "expires" DATETIME,
+    "expires" TIMESTAMP WITHOUT TIME ZONE, -- Sostituisce DATETIME
     "accessToken" TEXT NOT NULL,
     "userId" BIGINT,
     "firstName" TEXT,
@@ -14,5 +31,7 @@ CREATE TABLE "Session" (
     "accountOwner" BOOLEAN NOT NULL DEFAULT false,
     "locale" TEXT,
     "collaborator" BOOLEAN DEFAULT false,
-    "emailVerified" BOOLEAN DEFAULT false
+    "emailVerified" BOOLEAN DEFAULT false,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id") -- Definizione esplicita della chiave primaria
 );
